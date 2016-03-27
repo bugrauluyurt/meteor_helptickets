17b158d8e4749a1c61f045bb531ca72c2a2889af
bugrauluyurt <bugra.u@gmail.com>
2016-03-27 16:03:54 +0300

    Staff section design completed

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,3 +1,10 @@
+// Check if staff
+Template.registerHelper('isStaff', function(){
+	if (Meteor.user().profile.usertype == 'staff') {
+		return true;
+	};
+});
+
 // Format the date using momentjs
 Template.registerHelper('formatDate', function (date) {
 	return moment(date).format('MMMM Do YYYY, h:mm:ss a');
